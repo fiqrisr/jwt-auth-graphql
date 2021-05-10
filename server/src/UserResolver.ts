681d0c20fa0dfcdef64757a9c7d830a4ec09f8d2
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-10 08:00:18 +0700

    Add login

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,12 +1,31 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import {
+	Arg,
+	Field,
+	Mutation,
+	ObjectType,
+	Query,
+	Resolver
+} from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
 import { User } from './entity/User';
+import { sign } from 'jsonwebtoken';
+
+@ObjectType()
+class LoginResponse {
+	@Field()
+	accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
 	@Query(() => String)
 	hello() {
 		return 'hi!';
+	}
+
+	@Query(() => [User])
+	users() {
+		return User.find();
 	}
 
 	@Mutation(() => Boolean)
@@ -25,5 +44,27 @@ export class UserResolver {
 			console.log(err);
 			return false;
 		}
+
+		return true;
+	}
+
+	@Mutation(() => LoginResponse)
+	async login(
+		@Arg('email', () => String) email: string,
+		@Arg('password', () => String) password: string
+	): Promise<LoginResponse> {
+		const user = await User.findOne({ where: { email } });
+
+		if (!user) throw new Error('Invalid credentials');
+
+		const valid = await compare(password, user.password);
+
+		if (!valid) throw new Error('Invalid credentials');
+
+		return {
+			accessToken: sign({ userId: user.id }, 'jdslafjbuaidj', {
+				expiresIn: '15m'
+			})
+		};
 	}
 }
