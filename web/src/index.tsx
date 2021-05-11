2c27444f2641ee26746cf963842269fd671df50b
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-11 07:53:55 +0700

    Add auto refresh token

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -9,11 +9,14 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
-import { getAccessToken } from './accessToken';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import jwtDecode from 'jwt-decode';
+import { getAccessToken, setAccessToken } from './accessToken';
 import App from './App';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:4000/graphql'
+	uri: 'http://localhost:4000/graphql',
+	credentials: 'include'
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -39,10 +42,43 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 	if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
+const tokenRefreshLink = new TokenRefreshLink({
+	accessTokenField: 'accessToken',
+	isTokenValidOrUndefined: () => {
+		const token = getAccessToken();
+
+		if (!token) return true;
+
+		try {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			const { exp } = jwtDecode(token);
+
+			if (Date.now() >= exp * 1000) return false;
+
+			return true;
+		} catch {
+			return false;
+		}
+	},
+	fetchAccessToken: () => {
+		return fetch('http://localhost:4000/refresh_token', {
+			method: 'POST',
+			credentials: 'include'
+		});
+	},
+	handleFetch: accessToken => {
+		setAccessToken(accessToken);
+	},
+	handleError: err => {
+		console.warn('Your refresh token is invalid. Try to relogin');
+		console.error(err);
+	}
+});
+
 const client = new ApolloClient({
-	link: from([errorLink, authLink.concat(httpLink)]),
-	cache: new InMemoryCache(),
-	credentials: 'include'
+	link: from([tokenRefreshLink, errorLink, authLink.concat(httpLink)]),
+	cache: new InMemoryCache()
 });
 
 ReactDOM.render(
